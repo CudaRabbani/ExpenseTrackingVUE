3630872180365b9148dc1d89c734d43783bbc151
Reza Rabbani <reza.rabbani@horizn.com>
2024-05-20 16:39:21 -0600

    Initial Setup is done

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>History</h1>
+    <ul id="list" class="list">
+        <li 
+        v-for="transaction of this.transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus' ">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span>
+            <button class="delete-btn">X</button>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            transactions: [
+                {id: 1, text: 'Flower', amount: -19.99},
+                {id: 2, text: 'Fruits', amount: -29.99},
+                {id: 3, text: 'Vegetables', amount: -9.99},
+                {id: 4, text: 'Books', amount: -39.99},
+                {id: 5, text: 'Salary', amount: 200.00}
+            ],
+        }
+    }
+}
+</script>
