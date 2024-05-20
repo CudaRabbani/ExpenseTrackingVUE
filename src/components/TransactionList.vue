142fbdfa0feb12fa57b6cada82e6906f1e0d9291
Reza Rabbani <reza.rabbani@horizn.com>
2024-05-20 16:58:29 -0600

    Using the new composition API

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,7 @@
     <h1>History</h1>
     <ul id="list" class="list">
         <li 
-        v-for="transaction of this.transactions"
+        v-for="transaction of transactions"
         :key="transaction.id"
         :class="transaction.amount < 0 ? 'minus' : 'plus' ">
             {{ transaction.text }} <span>${{ transaction.amount }}</span>
@@ -10,18 +10,47 @@
         </li>
     </ul>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            transactions: [
-                {id: 1, text: 'Flower', amount: -19.99},
-                {id: 2, text: 'Fruits', amount: -29.99},
-                {id: 3, text: 'Vegetables', amount: -9.99},
-                {id: 4, text: 'Books', amount: -39.99},
-                {id: 5, text: 'Salary', amount: 200.00}
-            ],
-        }
-    }
-}
+//<script>
+// VueJS@2.0 way using the OptionsAPI
+// export default {
+//     data() {
+//         return {
+//             transactions: [
+//                 {id: 1, text: 'Flower', amount: -19.99},
+//                 {id: 2, text: 'Fruits', amount: -29.99},
+//                 {id: 3, text: 'Vegetables', amount: -9.99},
+//                 {id: 4, text: 'Books', amount: -39.99},
+//                 {id: 5, text: 'Salary', amount: 200.00}
+//             ],
+//         }
+//     }
+// }
+
+//VueJS @3.0 using the CompositionAPI
+// export default {
+//     setup() {
+//         const transactions = [
+//                 {id: 1, text: 'Flower', amount: -19.99},
+//                 {id: 2, text: 'Fruits', amount: -29.99},
+//                 {id: 3, text: 'Vegetables', amount: -9.99},
+//                 {id: 4, text: 'Books', amount: -39.99},
+//                 {id: 5, text: 'Salary', amount: 200.00}
+//             ];
+        
+//         return {
+//             transactions
+//         }
+//     }
+// }
+// </script>
+
+<script setup>
+const transactions = [
+        {id: 1, text: 'Flower', amount: -19.99},
+        {id: 2, text: 'Fruits', amount: -29.99},
+        {id: 3, text: 'Vegetables', amount: -9.99},
+        {id: 4, text: 'Books', amount: -39.99},
+        {id: 5, text: 'Salary', amount: 200.00}
+    ];
+
 </script>
