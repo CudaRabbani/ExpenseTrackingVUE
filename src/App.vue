87555c4f25cbffa930849617fb2bce9df1976167
Reza Rabbani <reza.rabbani@horizn.com>
2024-05-20 19:20:45 -0600

    Props passing, Form handling and Toast is added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpense />
-    <TransactionList />
+    <Balance :total="total" />
+    <IncomeExpense :income="income" :expense="expense"/>
+    <TransactionList :transactions="transactions"/>
     <AddTransaction />
   </div>
 </template>
-<script>
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomExpense from './components/IncomeExpense.vue';
@@ -15,14 +15,33 @@ import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction
-  }
-}
+import {ref, computed} from 'vue';
 
+const transactions = ref([
+        {id: 1, text: 'Flower', amount: -19.99},
+        {id: 2, text: 'Fruits', amount: -29.99},
+        {id: 3, text: 'Vegetables', amount: -9.99},
+        {id: 4, text: 'Books', amount: -39.99},
+        {id: 5, text: 'Salary', amount: 200.00}
+    ]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, income) => acc+income.amount, 0).toFixed(2);
+})
+
+const expense = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount < 0)
+  .reduce((acc, income) => acc+income.amount, 0).toFixed(2);
+})
+
+console.log('income expense ', income.value, expense.value);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -10,47 +10,13 @@
         </li>
     </ul>
 </template>
-//<script>
-// VueJS@2.0 way using the OptionsAPI
-// export default {
-//     data() {
-//         return {
-//             transactions: [
-//                 {id: 1, text: 'Flower', amount: -19.99},
-//                 {id: 2, text: 'Fruits', amount: -29.99},
-//                 {id: 3, text: 'Vegetables', amount: -9.99},
-//                 {id: 4, text: 'Books', amount: -39.99},
-//                 {id: 5, text: 'Salary', amount: 200.00}
-//             ],
-//         }
-//     }
-// }
-
-//VueJS @3.0 using the CompositionAPI
-// export default {
-//     setup() {
-//         const transactions = [
-//                 {id: 1, text: 'Flower', amount: -19.99},
-//                 {id: 2, text: 'Fruits', amount: -29.99},
-//                 {id: 3, text: 'Vegetables', amount: -9.99},
-//                 {id: 4, text: 'Books', amount: -39.99},
-//                 {id: 5, text: 'Salary', amount: 200.00}
-//             ];
-        
-//         return {
-//             transactions
-//         }
-//     }
-// }
-// </script>
-
 <script setup>
-const transactions = [
-        {id: 1, text: 'Flower', amount: -19.99},
-        {id: 2, text: 'Fruits', amount: -29.99},
-        {id: 3, text: 'Vegetables', amount: -9.99},
-        {id: 4, text: 'Books', amount: -39.99},
-        {id: 5, text: 'Salary', amount: 200.00}
-    ];
+    import {defineProps} from 'vue';
 
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            required: true,
+        },
+    });
 </script>
